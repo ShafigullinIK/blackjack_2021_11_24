fc1c9581e7ced05941124a0bb44ac3cb0cbcad9f
Tina Shumilova <shutiva@gmail.com>
2021-12-04 19:15:35 +0200

    final version

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿/* Задачи
-1. Создаем колоду карт.
-2, Для игры нам необходима перемешанная колода, поэтому надо в случайном порядке 
-перетасовать карты (или сделать какой-то механизм, чтобы менять порядок карт)
-*/
+﻿//Игра - финальный вариант
 int numberOfShuffle = 100;
 string[] cardSuit = new string[] { "треф", "пик", "буби", "червы" };
 string[] cardRank = new string[]
@@ -26,7 +22,6 @@ string[] FillCardDesk(string[] rank, string[] suit)
     }
     return shuffledDesk;
 }
-
 string[] DeskShuffle(string[] array, int number)
 {
     string temp = String.Empty;
@@ -46,25 +41,48 @@ string[] DeskShuffle(string[] array, int number)
     }
     return array;
 }
-
-string RandomSuit(string[] array)
+void PlayerKart(string[] kartArray, out string[] player1, out string[] player2)
 {
-    int i = new Random().Next(0, array.Length);
-    string text = array[i];
-    return text;
-}
-
-void PrintCards(string[] array) //string[] arraySuit) // вывод колоды на консоль
-{
-    for (int j = 0; j < array.Length; j++)
+    player1 = new string[10];
+    for (int i = 0; i < player1.Length; i++)
     {
-        //string suit = RandomSuit(arraySuit);
-        //Console.Write($"{array[j]} {suit}, ");
-        Console.Write($"{array[j]}, ");
+        player1[i] = "0";
+    }
+    player2 = new string[10];
+    for (int i = 0; i < player1.Length; i++)
+    {
+        player2[i] = "0";
+    }
+    player1[0] = kartArray[0];
+    player2[0] = kartArray[1];
+    player1[1] = kartArray[2];
+    player2[1] = kartArray[3];
+}
+void Print(string[] array, int b)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] != "0") Console.Write(array[i] + " ");
     }
     Console.WriteLine();
 }
+int AddKart(string[] kartArray, string[] player, int b, int j)
+{
+    string a = String.Empty;
+    Console.Write($"Игрок {b}, ещё карту надо? Y/N ");
 
+    for (int i = 2; a != "N"; i++)
+    {
+        a = Console.ReadLine();
+        if (a == "y" || a == "Y")
+        {
+            player[i] = kartArray[j++];
+            Console.Write($"Выпала карта: {player[i]}. Ещё? ");
+        }
+        if (a == "n" || a == "N") break;
+    }
+    return j;
+}
 int GetSumCard(string[] array)
 {
     int sum = 0;
@@ -85,164 +103,77 @@ int GetSumCard(string[] array)
     }
     return sum;
 }
-
-string[] CardDrawUser(string[] deck)
-{
-    string[] cardUser = new string[2];
-    for (int i = 0; i < cardUser.Length; i++)
-    {
-        cardUser[i] = deck[i];
-    }
-    return cardUser;
-}
-string[] CardDrawСroupier(string[] deck)
-{
-    string[] cardСroupier = new string[2];
-    int j = 2;
-    for (int i = 0; i < cardСroupier.Length; i++)
-    {
-        cardСroupier[i] = deck[j++];
-    }
-    return cardСroupier;
-}
-
 string FindWinner(int result1, int result2)
 {
     string winner = String.Empty;
     if (result1 == result2) winner = "Пуш";
-    if (result1 == 21) winner = "Блек-джек. Игрок победил";
-    if (result1 > 21 && result2 < 21) winner = "Перебор, выйграло казино";
-    if (result2 == 21) winner = "Игрок проиграл";
-    if (result1 > result2 && result1 != 21) winner = "Игрок победил";
-    if (result1 < result2) winner = "Игрок проиграл";
+    if (result1 == 21) winner = "Блек-джек. Игрок 1 победил";
+    if (result1 > 21 && result2 < 21) winner = "Перебор, Игрок 2 победил";
+    if (result2 == 21) winner = "Блек-джек. Игрок 2 победил";
+    if (result2 > 21 && result1 < 21) winner = "Перебор, Игрок 1 победил";
+    if (result1 > result2 && result1 != 21 && result1 < 21) winner = "Игрок 1 победил";
+    if (result2 > result1 && result2 != 21 && result2 < 21) winner = "Игрок 2 победил";
     return winner;
 }
-
-// Игра
-//string[] cardDesk = FillCardDesk(cardRank, cardSuit);
-
-//string[] shuffledCardDesk = DeskShuffle(cardDesk, numberOfShuffle);
-//PrintCards(shuffledCardDesk, cardSuit);
-
-//string[] playersHand = CardDrawUser(shuffledCardDesk);
-//PrintCards(playersHand);
-
-//string[] secondPlayersHand = PhaseGetCardUser(shuffledCardDesk, playersHand);
-
-//string[] croupierHand = CardDrawСroupier(shuffledCardDesk);
-//PrintCards(croupierHand);
-/*
-string[] secondCroupierHand = PhaseGetCardUser(shuffledCardDesk, croupierHand);
-
-int playerResult = GetSumCard(playersHand);
-int croupierResult = GetSumCard(croupierHand);
-
-Console.WriteLine(playerResult);
-Console.WriteLine(croupierResult);
-
-//5. Определение победителя в этом раунде.
-
-string gameResult = FindWinner(playerResult, croupierResult);
-Console.WriteLine(gameResult);
-*/
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine();
-
-
-
-
-string[] PhaseGetCardUser(string[] deck, string[] cardUser)
+int Answer()
 {
-    int indexCard = 4;
-    PrintCards(cardUser);
-    while (indexCard > 0)
+    Console.WriteLine("Сколько раундов будем играть?");
+    int round = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Играем {round} раунд{Ending(round)}");
+    return round;
+}
+string Ending(int number)
+{
+    string ending = String.Empty;
+    if(number == 1)
     {
-        System.Console.Write("Вам нужна еще карта? Введите Yes/No: ");
+        ending = " ";
+    }
+    else if (number == 2 || number == 3)
+    {
+        ending = "а";
+    }
+    else 
+    {
+        ending = "ов";
+    }
+    return ending;
+}
+
+Console.WriteLine("Сыграем в Black Jack?");
+Thread.Sleep(1000);
+int round = Answer();
+while (round >= 0)
+{
+    if (round == 0)
+    {
+        Console.WriteLine("Ещё играем? Y/N ");
         string answer = Console.ReadLine();
-        if (answer == "Yes" || answer == "yes")
+        if (answer == "y" || answer == "Y")
         {
-            Array.Resize(ref cardUser, cardUser.Length + 1);
-            cardUser[cardUser.Length - 1] = deck[indexCard];
-            PrintCards(cardUser);
-            indexCard++;
+            round = Answer();
         }
-        else break;
+        if (answer == "n" || answer == "N") break;
     }
-    return cardUser;
-}
-
-
-
-
-void PlayerKart(string[] kartArray, out string[] player1, out string[] player2)
-{
-    player1 = new string[10];
-    for (int i = 0; i < player1.Length; i++)
-    {
-        player1[i] = "0";
-    }
-    player2 = new string[10];
-    for (int i = 0; i < player1.Length; i++)
-    {
-        player2[i] = "0";
-    }
-    player1[0] = kartArray[0];
-    player2[0] = kartArray[1];
-    player1[1] = kartArray[2];
-    player2[1] = kartArray[3];
-}
-
-int AddKart(string[] kartArray, string[] player, int b, int j)
-{
-    string a = String.Empty;
-    Console.Write($"Игрок {b}, ещё карту надо? Y/N ");
-
-    for (int i = 2; a != "N"; i++)
-    {
-        a = Console.ReadLine();
-        if (a == "y" || a == "Y")
-        {
-            //Array.Resize(ref player, player.Length + 1);
-            player[i] = kartArray[j++];
-            Console.Write($"Выпала карта: {player[i]}. Ещё? ");
-        }
-        if (a == "n" || a == "N") break;
-    }
-    return j;
-}
-
-void Print(string[] array, int b)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] != "0") Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
-int round = 2;
-while (round > 0)
-{
     string[] cardDesk = FillCardDesk(cardRank, cardSuit);
     string[] shuffledCardDesk = DeskShuffle(cardDesk, numberOfShuffle);
     string[] playersHand;
     string[] croupierHand;
-    int b1 = 1;
-    int b2 = 2;
+    int player1 = 1;
+    int player2 = 2;
     int j = 4;
-    Console.WriteLine(j);
     PlayerKart(shuffledCardDesk, out playersHand, out croupierHand);
     Console.Write("Игрок 1 ваши карты: ");
-    Print(playersHand, b1);
+    Print(playersHand, player1);
     Console.Write("Игрок 2 ваши карты: ");
-    Print(croupierHand, b2);
+    Print(croupierHand, player2);
     Console.WriteLine();
-    j = AddKart(shuffledCardDesk, playersHand, b1, j);
+    j = AddKart(shuffledCardDesk, playersHand, player1, j);
     Console.Write("Итого у игрока 1: ");
-    Print(playersHand, b1);
-    AddKart(shuffledCardDesk, croupierHand, b2, j);
+    Print(playersHand, player1);
+    AddKart(shuffledCardDesk, croupierHand, player2, j);
     Console.Write("Итого у игрока 2:");
-    Print(croupierHand, b2);
+    Print(croupierHand, player2);
     int playerResult = GetSumCard(playersHand);
     int croupierResult = GetSumCard(croupierHand);
     Console.WriteLine(playerResult);
@@ -251,6 +182,8 @@ while (round > 0)
     string gameResult = FindWinner(playerResult, croupierResult);
     Console.WriteLine(gameResult);
     round--;
+    Console.WriteLine($"Осталось {round} раунд{Ending(round)}!");
+    Thread.Sleep(2000);
 }
 
 
